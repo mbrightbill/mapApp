65f7fdfb9cc56d811d257da9dfa5917dbc7388f4
mbrightbill <brightbill.mt@gmail.com>
2014-11-03 14:24:05 -0800

    Added Day 1 functionality

--- mapApp/MapViewController.swift
+++ mapApp/MapViewController.swift
@@ -9,13 +9,66 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.locationManager.delegate = self
+        self.mapView.delegate = self
         
-        let locationManager = 
+        let longPress = UILongPressGestureRecognizer(target: self, action: "didLongPressMap:")
+        self.mapView.addGestureRecognizer(longPress)
+        
+        switch CLLocationManager.authorizationStatus() as CLAuthorizationStatus {
+        case .Authorized:
+            println("authorized")
+            self.mapView.showsUserLocation = true
+        case .NotDetermined:
+            println("not determined")
+            self.locationManager.requestAlwaysAuthorization()
+        case .Denied:
+            println("denied")
+        case .Restricted:
+            println("restricted")
+        default:
+            println("default case")
+        }
+        
 
+    }
+    
+    func didLongPressMap(sender : UILongPressGestureRecognizer) {
+        
+        if sender.state == UIGestureRecognizerState.Began {
+            let touchPoint = sender.locationInView(self.mapView)
+            let touchCoordinate = self.mapView.convertPoint(touchPoint, toCoordinateFromView: self.mapView)
+            
+            var annotation = MKPointAnnotation()
+            annotation.coordinate = touchCoordinate
+            annotation.title = "Add Reminder"
+            self.mapView.addAnnotation(annotation)
+        }
+    }
+    
+    func mapView(mapView: MKMapView!, annotationView view: MKAnnotationView!, calloutAccessoryControlTapped control: UIControl!) {
+        
+        let reminderVC = self.storyboard?.instantiateViewControllerWithIdentifier("REMINDER_VC") as AddReminderViewController
+        reminderVC.locationManager = self.locationManager
+        reminderVC.selectedAnnotation = view.annotation
+        self.presentViewController(reminderVC, animated: true, completion: nil)
+    }
+    
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        
+        let annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "ANNOTATION")
+        annotationView.animatesDrop = true
+        annotationView.canShowCallout = true
+        let addButton = UIButton.buttonWithType(UIButtonType.ContactAdd) as UIButton
+        annotationView.rightCalloutAccessoryView = addButton
+        return annotationView
     }
 
     override func didReceiveMemoryWarning() {
