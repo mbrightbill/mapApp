2ac4d912b5520b62f1e0ed55d4a9b448321ee6b7
mbrightbill <brightbill.mt@gmail.com>
2014-11-05 22:57:22 -0800

    Added iCloud with CoreData functionality

--- mapApp/AddReminderViewController.swift
+++ mapApp/AddReminderViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 import MapKit
+import CoreData
 
 class AddReminderViewController: UIViewController {
     
     var locationManager : CLLocationManager!
     var selectedAnnotation : MKAnnotation!
+    var managedObjectContext : NSManagedObjectContext!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,13 +22,27 @@ class AddReminderViewController: UIViewController {
         let regionSet = self.locationManager.monitoredRegions
         let regions = regionSet.allObjects
         
+        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        self.managedObjectContext = appDelegate.managedObjectContext
+        
     }
 
  
     @IBAction func didPressAddReminderButton(sender: AnyObject) {
         
-        var geoRegion = CLCircularRegion(center: selectedAnnotation.coordinate, radius: 400_000.0, identifier: "TestRegion")
+        var geoRegion = CLCircularRegion(center: selectedAnnotation.coordinate, radius: 400_000.0, identifier: "Geo Region!")
         self.locationManager.startMonitoringForRegion(geoRegion)
+        
+        //insert a new reminder into our DataBase
+        var newReminder = NSEntityDescription.insertNewObjectForEntityForName("Reminder", inManagedObjectContext: self.managedObjectContext) as Reminder
+        newReminder.name = "Geo Region!"
+        
+        // save and check for error
+        var error : NSError?
+        self.managedObjectContext.save(&error)
+        if error != nil {
+            println("error: \(error?.localizedDescription)")
+        }
         
         NSNotificationCenter.defaultCenter().postNotificationName("REMINDER_ADDED", object: self, userInfo: ["region": geoRegion])
         self.dismissViewControllerAnimated(true, completion: nil)
--- mapApp/ReminderTableViewController.swift
+++ mapApp/ReminderTableViewController.swift
@@ -7,29 +7,53 @@
 //
 
 import UIKit
+import CoreData
 
-class ReminderTableViewController: UIViewController {
+class ReminderTableViewController: UIViewController, UITableViewDataSource, NSFetchedResultsControllerDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var managedObjectContext : NSManagedObjectContext!
+    var fetchedResultsController : NSFetchedResultsController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.tableView.dataSource = self
+        
+        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        self.managedObjectContext = appDelegate.managedObjectContext
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "didGetCloudChanges:", name: NSPersistentStoreDidImportUbiquitousContentChangesNotification, object: appDelegate.persistentStoreCoordinator)
+        
+        var fetchRequest = NSFetchRequest(entityName: "Reminder")
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+        
+        self.fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.managedObjectContext, sectionNameKeyPath: nil, cacheName: "Reminders")
+        self.fetchedResultsController?.delegate = self
+        var error : NSError?
+        if self.fetchedResultsController?.performFetch(&error) == false {
+            println("error: \(error?.localizedDescription)")
+        }
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func didGetCloudChanges(notification : NSNotification) {
+        self.managedObjectContext.mergeChangesFromContextDidSaveNotification(notification)
     }
-    */
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.fetchedResultsController?.fetchedObjects?.count ?? 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("REMINDER_CELL", forIndexPath: indexPath) as UITableViewCell
+        let reminder = self.fetchedResultsController?.fetchedObjects?[indexPath.row] as Reminder
+        
+        cell.textLabel.text = reminder.name
+        return cell
+    }
+    
+    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        self.tableView.reloadData()
+    }
 }
